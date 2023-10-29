46b38a9bd6259d5fcd81fb870f572eb7941c41b0
José Hernández <hernandezmorenojosemiguel@gmail.com>
2023-10-29 14:53:24 -0400

    Added a function to pull data from OPGG

--- commands/Lol/mySummoner.js
+++ commands/Lol/mySummoner.js
@@ -2,7 +2,7 @@ const { default: axios } = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { riotKey } = require('../../utils/riotApi');
 const { getSummoner } = require('../../utils/dbUtils');
-
+const { JSDOM } = require('jsdom');
 
 const createEmbed = (name, level, iconUrl) => {
   const exampleEmbed = new EmbedBuilder()
@@ -13,6 +13,22 @@ const createEmbed = (name, level, iconUrl) => {
       { name: 'Level', value: `${level}`, inline: true },
     );
   return exampleEmbed;
+};
+
+const callSummonerData = async (summoner) => {
+  const url = `https://www.op.gg/summoners/lan/${summoner}`;
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
+    // Crear un documento JSDOM para analizar el HTML
+    const { window } = new JSDOM(html);
+    const document = window.document;
+    // Supongamos que deseas extraer el nombre del invocador, que se encuentra en un elemento con una clase específica.
+    const summonerName = document.querySelector('.tier').textContent; // Reemplaza '.summoner-name' con el selector correcto.
+    console.log('Nombre del invocador:', summonerName);
+  } catch (error) {
+    console.error('Error al realizar la solicitud HTTP:', error);
+  }
 };
 
 
@@ -27,34 +43,41 @@ module.exports = {
       const version = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
       const versionActual = version.data[0];
       const discordId = interaction.user.id;
-      const summoner_name = getSummoner(discordId);
+      console.log(discordId);
+
+      // Añadir comprobación para verificar si summoner_name es válido.
+      const summoner_name = await getSummoner(discordId);
+      callSummonerData(summoner_name);
       console.log(summoner_name);
+
       if (!summoner_name) {
-        await interaction.reply('No tienes ningun invocador asociado a tu cuenta');
+        await interaction.reply('No tienes ningún invocador asociado a tu cuenta');
       } else {
         const response = await axios.get(`${dataGeneral}/${summoner_name.summoner}`, {
           headers: {
             'X-Riot-Token': api_key,
           },
         });
+        const summonerData = response.data;
+        const name = summonerData.name;
+        const level = summonerData.summonerLevel;
+        const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${summonerData.profileIconId}.png`;
+        const embed = createEmbed(name, level, iconUrl);
 
-        if (response.status === 200) {
-          const summonerData = response.data;
-          const name = summonerData.name;
-          const level = summonerData.summonerLevel;
-          const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${summonerData.profileIconId}.png`;
-          const matchlistUrl = `https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/${name}`;
-          console.log(matchlistUrl);
-          const embed = createEmbed(name, level, iconUrl);
-          await interaction.reply({ embeds: [embed] });
-        } else {
-          console.error('error');
-          await interaction.reply('Error al obtener datos del invocador.');
-        }
+        // Respondiendo con el embed creado.
+        await interaction.reply({ embeds: [embed] });
       }
     } catch (error) {
-      console.error('Error:', error);
-    }
+      console.error(error);
 
+      // Agregar manejo de errores para Axios.
+      if (error.isAxiosError) {
+        if (error.response) {
+          console.error('Respuesta de la API:', error.response.status, error.response.statusText);
+        } else {
+          console.error('No se pudo conectar a la API');
+        }
+      }
+    }
   },
 };
