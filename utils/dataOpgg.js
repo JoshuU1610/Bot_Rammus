0bf64bb07155b16fc13850ac937fdaadd80382b6
José Hernández <hernandezmorenojosemiguel@gmail.com>
2023-10-30 16:23:27 -0400

    Changing the function that calls OPGG data

--- utils/dataOpgg.js
+++ utils/dataOpgg.js
@@ -1,22 +1,38 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
-const getDataSummoner = async (username) => {
-  const url = `https://www.op.gg/summoners/lan/${username}`;
-  const response = await axios.get(url);
-  if (response.status === 200) {
-    const html = response.data;
-    const $ = cheerio.load(html);
-    console.log($);
-    // Ejemplo de extracción de información, como el nombre del jugador
-    // const player_name = $('.Summoner').text().trim();
-
-    // Puedes agregar más lógica para extraer la información que necesites
-
-    // console.log(`Nombre del jugador: ${player_name}`);
-  } else {
-    console.error(`Error al acceder a la página. Código de estado: ${response.status}`);
+const callSummonerData = async (summoner) => {
+  const url = `https://www.op.gg/_next/data/K4R8Ttdg6rFA2Yod21Vgy/en_US/summoners/lan/${summoner}.json?region=lan&summoner=${summoner}`;
+  try {
+    const response = await axios.get(url);
+    const summonerDataOPGG = response.data;
+    const soloq = summonerDataOPGG.pageProps.data.league_stats[0];
+    const flex = summonerDataOPGG.pageProps.data.league_stats[1];
+    const dataSoloq = {
+      type: soloq.queue_info.queue_translate,
+      tier: soloq.tier_info.tier,
+      division: soloq.tier_info.division,
+      lp: soloq.tier_info.lp,
+      division_img: soloq.tier_info.tier_image_url,
+      win: soloq.win,
+      lose: soloq.lose
+    };
+    const dataFlex = {
+      type: flex.queue_info.queue_translate,
+      tier: flex.tier_info.tier,
+      division: flex.tier_info.division,
+      lp: flex.tier_info.lp,
+      division_img: flex.tier_info.tier_image_url,
+      win: flex.win,
+      lose: flex.lose
+    };
+    const finalData = {
+      dataSoloq,
+      dataFlex
+    };
+    return finalData;
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
   }
 };
 
-module.exports = { getDataSummoner };
+module.exports = { callSummonerData };
