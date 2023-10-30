cbf0a4dda49c5d92a0a00ce08ad74ffdd4f92233
José Hernández <hernandezmorenojosemiguel@gmail.com>
2023-10-30 16:24:28 -0400

    aggregate of data taken from OPGG

--- commands/Lol/otherSummoner.js
+++ commands/Lol/otherSummoner.js
@@ -1,15 +1,27 @@
 const { default: axios } = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { riotKey } = require('../../utils/riotApi');
+const { callSummonerData } = require('../../utils/dataOpgg');
 
 
-const createEmbed = (name, level, iconUrl) => {
+const createEmbed = (name, level, iconUrl, data) => {
+  console.log(data);
+  const nameSoloq = data.dataSoloq.type;
+  const nameFlex = data.dataFlex.type;
+  const eloSoloq = `${data.dataSoloq.tier} ${data.dataSoloq.division}`;
+  const lpSoloq = `${data.dataSoloq.lp} lp`;
+  const eloFlex = `${data.dataFlex.tier} ${data.dataFlex.division}`;
+  const lpFlex = `${data.dataFlex.lp} lp`;
   const exampleEmbed = new EmbedBuilder()
     .setColor('#EAEE19')
     .setTitle(name)
     .setThumbnail(iconUrl)
     .addFields(
       { name: 'Level', value: `${level}`, inline: true },
+      { name: `${nameSoloq}`, value: `${eloSoloq}`, inline: true },
+      { name: 'LP', value: `${lpSoloq}`, inline: true },
+      { name: `${nameFlex}`, value: `${eloFlex}`, inline: true },
+      { name: 'LP', value: `${lpFlex}`, inline: true },
     );
   return exampleEmbed;
 };
@@ -26,6 +38,7 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
+    await interaction.deferReply();
     try {
       const dataGeneral = 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
       const api_key = riotKey();
@@ -43,8 +56,9 @@ module.exports = {
         const name = summonerData.name;
         const level = summonerData.summonerLevel;
         const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${summonerData.profileIconId}.png`;
-        const embed = createEmbed(name, level, iconUrl);
-        await interaction.reply({ embeds: [embed] });
+        const finalData = await callSummonerData(name);
+        const embed = createEmbed(name, level, iconUrl, finalData);
+        await interaction.editReply({ embeds: [embed] });
       } else {
         console.error('Error al obtener datos del invocador.');
         await interaction.reply('Error al obtener datos del invocador.');
