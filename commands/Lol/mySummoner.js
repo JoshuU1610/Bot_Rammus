cd6d6967e8c301715d6319c785a8e6ff93147e23
José Hernández <hernandezmorenojosemiguel@gmail.com>
2023-10-30 16:24:04 -0400

    aggregate of data taken from OPGG

--- commands/Lol/mySummoner.js
+++ commands/Lol/mySummoner.js
@@ -2,57 +2,46 @@ const { default: axios } = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { riotKey } = require('../../utils/riotApi');
 const { getSummoner } = require('../../utils/dbUtils');
-const { JSDOM } = require('jsdom');
+const { callSummonerData } = require('../../utils/dataOpgg');
 
-const createEmbed = (name, level, iconUrl) => {
+const createEmbed = (name, level, iconUrl, data) => {
+  console.log(data);
+  const nameSoloq = data.dataSoloq.type;
+  const nameFlex = data.dataFlex.type;
+  const eloSoloq = `${data.dataSoloq.tier} ${data.dataSoloq.division}`;
+  const lpSoloq = `${data.dataSoloq.lp} lp`;
+  const eloFlex = `${data.dataFlex.tier} ${data.dataFlex.division}`;
+  const lpFlex = `${data.dataFlex.lp} lp`;
   const exampleEmbed = new EmbedBuilder()
     .setColor('#EAEE19')
     .setTitle(name)
     .setThumbnail(iconUrl)
     .addFields(
       { name: 'Level', value: `${level}`, inline: true },
+      { name: `${nameSoloq}`, value: `${eloSoloq}`, inline: true },
+      { name: 'LP', value: `${lpSoloq}`, inline: true },
+      { name: `${nameFlex}`, value: `${eloFlex}`, inline: true },
+      { name: 'LP', value: `${lpFlex}`, inline: true },
     );
   return exampleEmbed;
 };
-
-const callSummonerData = async (summoner) => {
-  const url = `https://www.op.gg/summoners/lan/${summoner}`;
-  try {
-    const response = await axios.get(url);
-    const html = response.data;
-    console.log(html);
-    // Crear un documento JSDOM para analizar el HTML
-    const { window } = new JSDOM(html);
-    const document = window.document;
-    // Supongamos que deseas extraer el nombre del invocador, que se encuentra en un elemento con una clase específica.
-    const summonerName = document.querySelector('body'); // Reemplaza '.summoner-name' con el selector correcto.
-    console.log(summonerName);
-  } catch (error) {
-    console.error('Error al realizar la solicitud HTTP:', error);
-  }
-};
-
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('mi-invocador')
     .setDescription('tus datos de invocador'),
   async execute(interaction) {
+    await interaction.deferReply();
     try {
       const dataGeneral = 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
       const api_key = riotKey();
       const version = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
       const versionActual = version.data[0];
       const discordId = interaction.user.id;
-      console.log(discordId);
-
-      // Añadir comprobación para verificar si summoner_name es válido.
       const summoner_name = await getSummoner(discordId);
-      callSummonerData(summoner_name);
-      console.log(summoner_name);
 
       if (!summoner_name) {
-        await interaction.reply('No tienes ningún invocador asociado a tu cuenta');
+        await interaction.editReply('No tienes ningún invocador asociado a tu cuenta');
       } else {
         const response = await axios.get(`${dataGeneral}/${summoner_name.summoner}`, {
           headers: {
@@ -63,10 +52,10 @@ module.exports = {
         const name = summonerData.name;
         const level = summonerData.summonerLevel;
         const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${summonerData.profileIconId}.png`;
-        const embed = createEmbed(name, level, iconUrl);
+        const finalData = await callSummonerData(summoner_name.summoner);
+        const embed = createEmbed(name, level, iconUrl, finalData);
 
-        // Respondiendo con el embed creado.
-        await interaction.reply({ embeds: [embed] });
+        await interaction.editReply({ embeds: [embed] });
       }
     } catch (error) {
       console.error(error);
