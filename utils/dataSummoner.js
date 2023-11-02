d34dd1122bf844163fd414a69ea7ffe508c5830a
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-11-01 21:52:50 -0400

    Creation of a function that makes it easy to bring the data needed from the caller for any request

---
+++ utils/dataSummoner.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const { riotKey } = require('./riotApi');
+
+const summonerData = async (summoner) => {
+  const url = `https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}`;
+  const api_key = riotKey();
+  try {
+    const response = await axios.get(`${url}`, {
+      headers: {
+        'X-Riot-Token': api_key,
+      },
+    });
+    const summonerData = response.data;
+    const data = {
+      name: summonerData.name,
+      level: summonerData.summonerLevel,
+      icon: summonerData.profileIconId,
+      summid: summonerData.id,
+      accountid: summonerData.accountId,
+      puuid: summonerData.puuid
+    };
+    return data;
+  } catch (error) {
+    console.error('Error al realizar la solicitud:', error);
+  }
+};
+
+module.exports = { summonerData };
