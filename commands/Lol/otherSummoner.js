67916fc7fe968e55129551bf4760276ac6c05d5d
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-11-01 21:58:17 -0400

    Add new functions for efficient calls

--- commands/Lol/otherSummoner.js
+++ commands/Lol/otherSummoner.js
@@ -1,7 +1,8 @@
 const { default: axios } = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { riotKey } = require('../../utils/riotApi');
-const { callSummonerData } = require('../../utils/dataOpgg');
+const { callSummonerData } = require('../../utils/dataRanked');
+const { summonerData } = require('../../utils/dataSummoner');
 
 
 const createEmbed = (name, level, iconUrl, data) => {
@@ -12,7 +13,7 @@ const createEmbed = (name, level, iconUrl, data) => {
 
   if (data.dataSoloq.tier === null || data.dataSoloq.division === null || data.dataSoloq.lp === null) {
     eloSoloq = 'Unranked';
-    lpSoloq = '0';
+    lpSoloq = 'Unranked';
   } else {
     eloSoloq = `${data.dataSoloq.tier} ${data.dataSoloq.division}`;
     lpSoloq = `${data.dataSoloq.lp} lp`;
@@ -20,19 +21,20 @@ const createEmbed = (name, level, iconUrl, data) => {
 
   if (data.dataFlex.tier === null || data.dataFlex.division === null || data.dataFlex.lp === null) {
     eloFlex = 'Unranked';
-    lpFlex = '0';
+    lpFlex = 'Unranked';
   } else {
     eloFlex = `${data.dataFlex.tier} ${data.dataFlex.division}`;
     lpFlex = `${data.dataFlex.lp} lp`;
   }
+
   const exampleEmbed = new EmbedBuilder()
     .setColor('#EAEE19')
     .setTitle(name)
     .setThumbnail(iconUrl)
     .addFields(
       { name: 'Level', value: `${level}` },
-      { name: `${nameSoloq}`, value: `${eloSoloq}  ${lpSoloq}` },
-      { name: `${nameFlex}`, value: `${eloFlex}  ${lpFlex}` },
+      { name: `${nameSoloq}`, value: `${eloSoloq} - ${lpSoloq}` },
+      { name: `${nameFlex}`, value: `${eloFlex} - ${lpFlex}` },
     );
   return exampleEmbed;
 };
@@ -51,29 +53,7 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply();
     try {
-      const dataGeneral = 'https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name';
-      const api_key = riotKey();
-      const summoner_name = interaction.options.getString('summoner');
-      const version = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
-      const versionActual = version.data[0];
-      const response = await axios.get(`${dataGeneral}/${summoner_name}`, {
-        headers: {
-          'X-Riot-Token': api_key,
-        },
-      });
 
-      if (response.status === 200) {
-        const summonerData = response.data;
-        const name = summonerData.name;
-        const level = summonerData.summonerLevel;
-        const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${summonerData.profileIconId}.png`;
-        const finalData = await callSummonerData(name);
-        const embed = createEmbed(name, level, iconUrl, finalData);
-        await interaction.editReply({ embeds: [embed] });
-      } else {
-        console.error('Error al obtener datos del invocador.');
-        await interaction.reply('Error al obtener datos del invocador.');
-      }
     } catch (error) {
       console.error('Error:', error);
     }
