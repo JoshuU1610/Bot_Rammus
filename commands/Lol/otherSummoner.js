05f81c092464b422a4a367c09ec2405a857db1c0
Joshu1610 <hernandezmorenojosemiguel@gmail.com>
2023-11-03 21:16:39 -0400

    Added the function that brings you the most played champions and how we search for the ranked ones

--- commands/Lol/otherSummoner.js
+++ commands/Lol/otherSummoner.js
@@ -1,12 +1,11 @@
 const { default: axios } = require('axios');
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { riotKey } = require('../../utils/riotApi');
 const { callSummonerData } = require('../../utils/dataRanked');
 const { summonerData } = require('../../utils/dataSummoner');
+const { masteryChampsSummoner } = require('../../utils/masteryChamps');
 
 
-const createEmbed = (name, level, iconUrl, data) => {
-  console.log(data);
+const createEmbed = (name, level, iconUrl, data, champ) => {
   const nameSoloq = data.dataSoloq.type;
   const nameFlex = data.dataFlex.type;
   let eloSoloq, lpSoloq, eloFlex, lpFlex;
@@ -35,6 +34,10 @@ const createEmbed = (name, level, iconUrl, data) => {
       { name: 'Level', value: `${level}` },
       { name: `${nameSoloq}`, value: `${eloSoloq} - ${lpSoloq}` },
       { name: `${nameFlex}`, value: `${eloFlex} - ${lpFlex}` },
+      { name: 'Info extra', value: 'campeones mas jugados' },
+      { name: `${champ[0].champion} - m${champ[0].mastery}`, value: `${champ[0].points} puntos` },
+      { name: `${champ[1].champion} - m${champ[1].mastery}`, value: `${champ[1].points} puntos` },
+      { name: `${champ[2].champion} - m${champ[2].mastery}`, value: `${champ[2].points} puntos` },
     );
   return exampleEmbed;
 };
@@ -53,7 +56,17 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply();
     try {
-
+      const summoner_name = interaction.options.getString('summoner');
+      const version = await axios.get('https://ddragon.leagueoflegends.com/api/versions.json');
+      const versionActual = version.data[0];
+      const dataSumm = await summonerData(summoner_name);
+      const champ = await masteryChampsSummoner(dataSumm.summ_id);
+      const name = dataSumm.name;
+      const level = dataSumm.level;
+      const iconUrl = `http://ddragon.leagueoflegends.com/cdn/${versionActual}/img/profileicon/${dataSumm.icon}.png`;
+      const finalData = await callSummonerData(dataSumm);
+      const embed = createEmbed(name, level, iconUrl, finalData, champ);
+      await interaction.editReply({ embeds: [embed] });
     } catch (error) {
       console.error('Error:', error);
     }
